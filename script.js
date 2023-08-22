2eb8a311d8e2a4f3a02b4da7fb1a646735e3faef
Kyle Salvador <kizutsuitemotachiagaru@gmail.com>
2023-08-22 20:07:59 +0800

    Added data to air conditions. Chance of rain still empty

--- script.js
+++ script.js
@@ -6,21 +6,35 @@ const requestParameter = '&q=London&aqi=no';
 const locationName = document.getElementById('location-name');
 const currentTemp = document.getElementById('temperature');
 const weatherCondition = document.getElementById('condition');
+const reelFeel = document.getElementById('reel-feel');
+const windSpeed = document.getElementById('wind-speed');
+const uvIndex = document.getElementById('uv-index');
 
 
 
-function callAPI () {
+function displayCurrentWeather() {
     fetch(URL + requestMethod + '?key=' + API_KEY + requestParameter)
     .then(response => response.json())
     .then(data => {
-
         console.log(data);
-
         locationName.textContent = data.location.name;
         currentTemp.textContent = data.current.temp_c + '°C';
         weatherCondition.textContent = data.current.condition.text;
+
+        reelFeel.textContent = 'Reel Feel: ' + data.current.feelslike_c + '°C';
+        windSpeed.textContent = 'Wind: ' + data.current.wind_kph;
+        uvIndex.textContent = 'UV Index: ' + data.current.uv;   
     });
 }
+
+displayCurrentWeather()
+
+
+
+
+
+
+
 
 
 
