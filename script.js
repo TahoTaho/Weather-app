a03ad6710714edb1b7177c33107860ffa1d4b01e
Kyle Salvador <kizutsuitemotachiagaru@gmail.com>
2023-08-23 17:57:16 +0800

    Chance of rain data displayed

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const URL = 'http://api.weatherapi.com/v1/';
 const API_KEY = '0e0ae5b7be654d2eb23112554231408';
-const requestMethod = 'current.json';
-const requestParameter = '&q=London&aqi=no';
+
 
 const locationName = document.getElementById('location-name');
 const currentTemp = document.getElementById('temperature');
@@ -9,10 +8,11 @@ const weatherCondition = document.getElementById('condition');
 const reelFeel = document.getElementById('reel-feel');
 const windSpeed = document.getElementById('wind-speed');
 const uvIndex = document.getElementById('uv-index');
+const chanceOfRain = document.getElementById('chance-of-rain');
 
 
 
-function displayCurrentWeather() {
+function displayCurrentWeather(requestMethod, requestParameter) {
     fetch(URL + requestMethod + '?key=' + API_KEY + requestParameter)
     .then(response => response.json())
     .then(data => {
@@ -22,12 +22,26 @@ function displayCurrentWeather() {
         weatherCondition.textContent = data.current.condition.text;
 
         reelFeel.textContent = 'Reel Feel: ' + data.current.feelslike_c + '°C';
-        windSpeed.textContent = 'Wind: ' + data.current.wind_kph;
+        windSpeed.textContent = 'Wind: ' + data.current.wind_kph + ' km/h';
+        chanceOfRain.textContent = 'Chance of Rain: ' + data.forecast.forecastday[0].day.daily_chance_of_rain;
         uvIndex.textContent = 'UV Index: ' + data.current.uv;   
     });
 }
 
-displayCurrentWeather()
+displayCurrentWeather('forecast.json', '&q=London&days=1&aqi=no&alerts=no');
+
+function displayForecastToday(requestMethod, requestParameter) {
+    fetch(URL + requestMethod + '?key=' + API_KEY + requestParameter)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        
+    });
+}
+
+//displayForecastToday('forecast.json', '&q=London&days=1&aqi=no&alerts=no');
+
+
 
 
 
